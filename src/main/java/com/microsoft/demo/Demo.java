55bc01bcc3911332de65e068886cbeac03fcfa0b
eirblaze <eirblaze@live.jp>
2019-07-14 19:51:24 +0900

    自動リリースをためす

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,10 +3,10 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
-            System.out.println("I am covered");
+            System.out.println("I am covered. release test.");
             return;
         }
 
-        System.out.println("I am not covered");
+        System.out.println("I am not covered. release test.");
     }
 }
